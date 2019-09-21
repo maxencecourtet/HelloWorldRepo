6b7c7f43ad52e97b751051cbd89e5d2995e08214
Maxence Courtet <maxence.courtet@epfl.ch>
2019-09-21 15:41:27 +0200

    changed twice

--- HellloWorld.java
+++ HellloWorld.java
@@ -1,5 +1,10 @@
 public class HellloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, SwEng!");
+        System.out.println("Hello, SwEng! \n");
+        System.out.println("The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer());
     }
 }
+
+    public static String computeUltimateAnswer() {
+        return "still searching...";
+    }
